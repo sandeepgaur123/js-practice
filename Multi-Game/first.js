967b2e49637a70d45c5b243679aad670b3d92f0e
sandeep <skg047496@gmail.com>
2023-02-09 21:49:02 +0530

    first challenge

---
+++ Multi-Game/first.js
@@ -0,0 +1,38 @@
+const qusEl = document.getElementById("qus");
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score");
+console.log(qusEl);
+
+
+
+let score = JSON.parse(localStorage.getItem("score"));
+scoreEl.innerText = `Score : ${score}`;
+const num1 = Math.floor(Math.random()*20);
+const num2 = Math.floor(Math.random()*10);
+const ans = num1 * num2;
+
+qusEl.innerText=`What will be ${num1} Multiply by ${num2} ?`;
+
+
+formEl.addEventListener("submit",(e)=>{
+   e.preventDefault();
+   const myans = +inputEl.value;
+   inputEl.value = '';
+   console.log(myans,typeof myans);
+   if(myans === ans){
+    score++;
+    setlLocalStorage();
+   }
+   else{
+    score--;
+    setlLocalStorage();
+   }
+})
+
+function setlLocalStorage(){
+    localStorage.setItem("score",JSON.stringify(score));
+}
+
+
+
